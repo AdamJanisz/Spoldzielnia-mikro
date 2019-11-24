d778f76a3aab059d43520ebed75b04cd01964407
mateusz <przykladowy@o3.pl>
2019-11-24 17:30:41 +0100

    initial

---
+++ src/main/java/com/adammateusz/spoldzielniamikro/SpoldzielniamikroApplication.java
@@ -0,0 +1,13 @@
+package com.adammateusz.spoldzielniamikro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoldzielniamikroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoldzielniamikroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adammateusz/spoldzielniamikro/SpoldzielniamikroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adammateusz.spoldzielniamikro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoldzielniamikroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
